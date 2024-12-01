5225583d13fa7e231ac05a99fdcc5e82c61b17af
Ethan <ethanhuang03@gmail.com>
2024-12-01 11:58:24 -0700

    Strip user input to ignore leading and trailign whitespace

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Select mode: GUI or TEXT?");
-        String mode = scanner.nextLine().toUpperCase();
+        String mode = scanner.nextLine().toUpperCase().strip();
 
         GameBoard model = new GameBoard();
         GameBoardController controller = new GameBoardController(model);
